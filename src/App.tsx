958231ab4691e0e2563a169b50fd00f649945bed
Natasa <tashomyy@gmail.com>
2025-03-11 11:23:51 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-xl my-5 mx-2 text-center pt-12">
+        Welcome to the IGaming project app
+      </h1>
+      <p className="text-center">Initial commit setup</p>
+      <img
+        src="/monkey-link.svg"
+        alt="Monkey logo"
+        className="w-4/12 mx-auto mt-5"
+      />
+    </>
+  );
+}
+
+export default App;
